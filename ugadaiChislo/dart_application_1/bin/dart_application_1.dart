269019bc4256d0dcec6fdfd40ad867eff507ba6a
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2022-12-19 00:48:34 +0600

    game

--- ugadaiChislo/dart_application_1/bin/dart_application_1.dart
+++ ugadaiChislo/dart_application_1/bin/dart_application_1.dart
@@ -2,74 +2,87 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  game();
+  String ans = "";
+  while (ans != "выход") {
+    print("Выберите режим");
+    print("Комп == 1");
+    print("Пользователь == 2");
+    print("Дуэль, первым начинает комп == 3");
+    ans = stdin.readLineSync()!;
+    if (ans == "1") {
+      comp();
+    } else if (ans == "2") {
+      gamer();
+    } else if (ans == "3") {
+      duel();
+    }
+  }
 }
 
-void game() {
+void comp() {
+  String ans = "";
+  int min = 0;
+  int max = 100;
+  int count = 1;
+  int mid = ((max + min) / 2).round();
+  while (ans != "yes") {
+    print("ваше число $mid?");
+    ans = stdin.readLineSync()!;
+
+    if (ans == "less") {
+      max = mid;
+      mid = (max + min) ~/ 2;
+      count++;
+
+      print(" ваше число $mid?");
+    } else if (ans == "greater") {
+      min = mid;
+      mid = ((max + min) / 2).round();
+      count++;
+    } else if (ans == "yes") {
+      print("конец игры");
+    } else {
+      print("Вы ввели некор данные, введите less/greater");
+      ans = stdin.readLineSync()!;
+    }
+  }
+  print("комп угадал за $count ходов");
+  print(" твое число $mid!");
+}
+
+//2 round
+void gamer() {
+  int counter = 1;
+
+  print("Угадай число)");
+  int randomNumber = Random().nextInt(100);
+
+  int ansNumber = int.parse(stdin.readLineSync()!);
+  while (ansNumber != randomNumber) {
+    if (ansNumber > randomNumber) {
+      print("less");
+      counter++;
+      ansNumber = int.parse(stdin.readLineSync()!);
+    } else if (ansNumber < randomNumber) {
+      print("greater");
+      counter++;
+      ansNumber = int.parse(stdin.readLineSync()!);
+    } else {
+      print("Вы ввели некор данные, введите less/greaterе");
+      ansNumber = int.parse(stdin.readLineSync()!);
+    }
+  }
+  print("конец игры, комп загадал $randomNumber");
+  print("пользователь угадал за $counter шагов");
+}
+
+//3
+void duel() {
   int min = 0;
   int max = 100;
   String ans = "";
   int count = 1;
   int counter = 1;
-
-  print("выберите режим");
-  print("комп == 1");
-  print("пользователь == 2");
-  print("дуэль == 3");
-  ans = stdin.readLineSync()!;
-  if (ans == "1") {
-    int mid = ((max + min) / 2).round();
-    while (ans != "yes") {
-      print("ваше число $mid?");
-      ans = stdin.readLineSync()!;
-
-      if (ans == "less") {
-        max = mid;
-        mid = (max + min) ~/ 2;
-        count++;
-
-        print(" ваше число $mid?");
-      } else if (ans == "greater") {
-        min = mid;
-        mid = ((max + min) / 2).round();
-        count++;
-      } else if (ans == "yes") {
-        print("конец игры");
-      } else {
-        print("Вы ввели некор данные, введите less/greater");
-        ans = stdin.readLineSync()!;
-      }
-    }
-    print("комп угадал за $count ходов");
-    print(" твое число $mid!");
-  }
-  //2 round
-  if (ans == "2") {
-    print("Угадай число)");
-    int randomNumber = Random().nextInt(100);
-
-    int ansNumber = int.parse(stdin.readLineSync()!);
-    while (ansNumber != randomNumber) {
-      if (ansNumber > randomNumber) {
-        print("less");
-        counter++;
-        ansNumber = int.parse(stdin.readLineSync()!);
-      } else if (ansNumber < randomNumber) {
-        print("greater");
-        counter++;
-        ansNumber = int.parse(stdin.readLineSync()!);
-      } else {
-        print("Вы ввели некор данные, введите less/greaterе");
-        ansNumber = int.parse(stdin.readLineSync()!);
-      }
-    }
-    print("конец игры, косп загадал $randomNumber");
-    print("пользователь угадал за $counter шагов");
-  }
-  //3
-  if (ans == "3") {
-    print("Дуэль, первым начинает комп");
-  }
   int mid = ((max + min) / 2).round();
 
   while (ans != "yes" && count < 3) {
@@ -87,8 +100,6 @@ void game() {
       count++;
     } else if (ans == "yes") {
       print("конец игры");
-    } else if (count <= 3) {
-      print("ходы закончились");
     } else {
       print("Вы ввели некор данные, введите less/greater");
       ans = stdin.readLineSync()!;
@@ -107,20 +118,18 @@ void game() {
       print("greater");
       counter++;
       ansNumber = int.parse(stdin.readLineSync()!);
-    } else if (counter <= 3) {
-      print("ходы закончились");
     } else {
-      print("Вы ввели некор данные, введите less/greaterе");
+      print("Вы ввели некор данные, введите less/greater");
       ansNumber = int.parse(stdin.readLineSync()!);
     }
   }
   print("конец игры, комп загадал $randomNumber");
-  print("Было $count шагов");
-  if (count > counter) {
+
+  if (count < counter) {
     print("$count:$counter, победил комп");
   } else if (count == counter) {
     print("$count:$counter, ничья");
-  } else if (count < counter) {
+  } else if (count > counter) {
     print("$count:$counter, победил пользователь");
   }
 }
